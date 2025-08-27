2a510b323ac4f1fce98ecb879c8f6e228463e30d
Eder Nuñez <santieder16@gmail.com>
2025-08-26 19:55:24 -0500

    actualizar implementacion

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
-use std::io::{BufRead, Write};
+use std::{
+    collections::BTreeSet,
+    error::Error,
+    fmt::Display,
+    io::{BufRead, Write},
+};
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug)]
+enum TuringMachineError {
+    Parse(String),
+    Transformation(String),
+    Args(String),
+    Io(std::io::Error),
+}
+
+impl From<std::io::Error> for TuringMachineError {
+    fn from(v: std::io::Error) -> Self {
+        Self::Io(v)
+    }
+}
+
+impl Error for TuringMachineError {}
+
+impl Display for TuringMachineError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Parse(s) | Self::Transformation(s) | Self::Args(s) => {
+                write!(f, "{s}")
+            }
+            Self::Io(error) => error.fmt(f),
+        }
+    }
+}
+
 enum Step {
     L,
     R,
 }
 
 impl TryFrom<&str> for Step {
-    type Error = String;
+    type Error = TuringMachineError;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value {
             "L" => Ok(Self::L),
             "R" => Ok(Self::R),
-            _ => Err(format!("{value} is not a valid step. Expected 'L' or 'R'",)),
+            _ => Err(TuringMachineError::Transformation(format!(
+                "{value} is not a valid step. Expected 'L' or 'R'"
+            ))),
         }
     }
 }
@@ -20,7 +53,6 @@ impl TryFrom<&str> for Step {
 type State<'a> = &'a str;
 type Symbol<'a> = &'a str;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Turd<'a> {
     current: State<'a>,
     read: Symbol<'a>,
@@ -30,65 +62,63 @@ struct Turd<'a> {
 }
 
 impl<'a> Turd<'a> {
-    const fn new(
-        current: State<'a>,
-        read: Symbol<'a>,
-        write: Symbol<'a>,
-        step: Step,
-        next: State<'a>,
-    ) -> Self {
-        Self {
-            current,
-            read,
-            write,
-            step,
-            next,
-        }
-    }
-
-    fn parse_turd(filepath: &str, s: (usize, &'a str)) -> Result<Self, String> {
+    fn parse_turd(filepath: &str, s: (usize, &'a str)) -> Result<Self, TuringMachineError> {
+        const CURRENT: usize = 0;
+        const READ: usize = 1;
+        const WRITE: usize = 2;
+        const STEP: usize = 3;
+        const NEXT: usize = 4;
         let tokens = s.1.split_whitespace().map(str::trim).collect::<Vec<_>>();
         if tokens.len() != 5 {
-            return Err(format!(
+            return Err(TuringMachineError::Parse(format!(
                 "{filepath}:{}: A single turd is expected to have 5 tokens",
                 s.0 + 1
-            ));
+            )));
         }
-        let current = 0;
-        let read = 1;
-        let write = 2;
-        let step = 3;
-        let next = 4;
-        Ok(Self::new(
-            tokens[current],
-            tokens[read],
-            tokens[write],
-            tokens[step].try_into()?,
-            tokens[next],
-        ))
+
+        Ok(Self {
+            current: tokens[CURRENT],
+            read: tokens[READ],
+            write: tokens[WRITE],
+            step: tokens[STEP].try_into()?,
+            next: tokens[NEXT],
+        })
     }
-    fn states_of_turds(turds: &[Self]) -> Box<[State]> {
-        let mut states = std::collections::BTreeSet::new();
-        for turd in turds {
-            states.insert(turd.current);
-            states.insert(turd.next);
-        }
-        states.iter().copied().collect()
+
+    fn states_of_turds(turds: &[Self]) -> impl Iterator<Item = State<'_>> {
+        turds
+            .iter()
+            .flat_map(|t| [t.current, t.next])
+            .collect::<BTreeSet<_>>()
+            .into_iter()
     }
 }
 
-#[derive(Debug)]
 struct Machine<'a> {
-    tape: Box<[Symbol<'a>]>,
+    tape: Vec<Symbol<'a>>,
     head: usize,
     state: State<'a>,
 }
 
-impl<'a> Machine<'a> {
-    const fn new(tape: Box<[Symbol<'a>]>, head: usize, state: State<'a>) -> Self {
-        Self { tape, head, state }
+impl Display for Machine<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "STATE: {}", self.state)?;
+        self.tape.iter().try_for_each(|cell| write!(f, "{cell} "))?;
+        writeln!(f)?;
+        for (i, cell) in self.tape.iter().enumerate() {
+            if i == self.head {
+                write!(f, "^")?;
+            }
+            (0..cell.len()).try_for_each(|_| write!(f, " "))?;
+            if i != self.head {
+                write!(f, " ")?;
+            }
+        }
+        writeln!(f)
     }
+}
 
+impl<'a> Machine<'a> {
     fn next(&mut self, program: &'a [Turd]) -> bool {
         for turd in program {
             if turd.current == self.state && turd.read == self.tape[self.head] {
@@ -104,67 +134,55 @@ impl<'a> Machine<'a> {
         }
         false
     }
-
-    fn dump(&self) {
-        println!("STATE: {}", self.state);
-        self.tape.iter().for_each(|cell| print!("{cell} "));
-        println!();
-        self.tape.iter().enumerate().for_each(|(i, cell)| {
-            if i == self.head {
-                print!("^");
-            }
-            (0..cell.len()).for_each(|_| print!(" "));
-            if i != self.head {
-                print!(" ");
-            }
-        });
-        println!();
-    }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = std::env::args().collect::<Box<[_]>>();
-    if args.len() < 3 {
+fn main() -> Result<(), TuringMachineError> {
+    let mut args = std::env::args();
+    let program = args.next().unwrap();
+    if args.len() < 2 {
         eprintln!("Error: input file is not provided");
-        return Err("Usage: .\turing-machine <input.turd> <input.tape>".into());
+        return Err(TuringMachineError::Args(format!(
+            "Usage: {program} <input.turd> <input.tape>"
+        )));
     }
-    let turd_filepath = &args[1];
-    let tape_filepath = &args[2];
+    let turd_filepath = &args.next().unwrap();
+    let tape_filepath = &args.next().unwrap();
 
     let content = std::fs::read_to_string(turd_filepath)?;
     let turds = content
         .lines()
         .map(str::trim)
         .enumerate()
-        .filter(|x| !x.1.is_empty())
-        .map(|x| Turd::parse_turd(turd_filepath, x).unwrap())
-        .collect::<Box<[_]>>();
+        .filter_map(|x| {
+            if x.1.is_empty() {
+                None
+            } else {
+                Some(Turd::parse_turd(turd_filepath, x))
+            }
+        })
+        .collect::<Result<Vec<_>, _>>()?;
 
     let states = Turd::states_of_turds(&turds);
 
     println!("Possible states:");
-    states.iter().for_each(|state| println!("{state}"));
+    states.for_each(|state| println!("{state}"));
     print!("Initial_state: ");
     std::io::stdout().flush()?;
     let initial_state = std::io::stdin().lock().lines().next().unwrap()?;
     println!();
 
     let binding = std::fs::read_to_string(tape_filepath)?;
-    let mut machine = Machine::new(
-        binding
-            .split_whitespace()
-            .map(str::trim)
-            .collect::<Box<[_]>>(),
-        0,
-        &initial_state,
-    );
+    let mut machine = Machine {
+        tape: binding.split_whitespace().map(str::trim).collect(),
+        head: 0,
+        state: &initial_state,
+    };
     loop {
-        machine.dump();
+        print!("{machine}");
         std::thread::sleep(std::time::Duration::from_millis(100));
         if !machine.next(&turds) {
             break;
         }
     }
-
     Ok(())
 }
